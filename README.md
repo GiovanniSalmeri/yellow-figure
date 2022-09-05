5d17570562da5b2aced2de879dd97ad01b6e59e9
Giovanni <g.salmeri@mondodomani.org>
2022-09-05 21:50:38 +0200

    First release

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# yellow-figure
-Images with captions
+# Figure 0.8.20
+
+Images with captions.
+
+<p align="center"><img src="figure-screenshot.png?raw=true" alt="Screenshot"></p>
+
+## How to add a caption to an image
+
+Use the shortcut `figure`. Arguments, functionality and settings are the same as for [Image](https://github.com/datenstrom/yellow-extensions/tree/master/source/image), except that the `Alt` argument is mandatory and used for the caption.
+
+## Examples
+
+Adding images with captions, with differents styles and sizes:
+
+    [figure photo.jpg "This is the caption of the image"]
+    [figure photo.jpg "This is the caption of the image" left]
+    [figure photo.jpg "This is the caption of the image" right 50%]
+    [figure photo.jpg "This is the caption of the image" right 320 200]
+
+## Installation
+
+[Download extension](https://github.com/GiovanniSalmeri/yellow-figure/archive/master.zip) and copy zip file into your `system/extensions` folder. Right click if you use Safari.
+
+This extension requires [Image](https://github.com/datenstrom/yellow-extensions/tree/master/source/image) by Datenstrom.
+
+## Developer
+
+Giovanni Salmeri. [Get help](https://github.com/GiovanniSalmeri/yellow-caption/issues).
---
+++ figure.php
@@ -0,0 +1,55 @@
+<?php
+// Figure extension, https://github.com/GiovanniSalmeri/yellow-figure
+
+class YellowFigure {
+    const VERSION = "0.8.20";
+    public $yellow;         // access to API
+    
+    // Handle initialisation
+    public function onLoad($yellow) {
+        $this->yellow = $yellow;
+    }
+
+    // Handle page content of shortcut
+    public function onParseContentShortcut($page, $name, $text, $type) {
+        $output = null;
+        if ($name=="figure" && $type=="inline") {
+            if ($this->yellow->extension->isExisting("image")) {
+                $imageElement = $this->yellow->extension->get("image")->onParseContentShortcut($page, "image", $text, $type);
+                if (preg_match('/ alt="([^"]+)"/', $imageElement, $matches)) {
+                    $caption = $matches[1];
+                    $captionId = "caption-".uniqid();
+                    $captionElement = "<figcaption id=\"".$captionId."\">".$caption."</figcaption>\n";
+                    preg_match('/ class="([^"]*)"/', $imageElement, $matches);
+                    $class = $matches ? " class=\"figure ".$matches[1]."\"" : "class=\"figure\"";
+                    preg_match('/ width="([^"]+)"/', $imageElement, $matches);
+                    $width = $matches ? $matches[1] : "";
+                    if (is_numeric($width)) $width .= "px";
+                    $imageElement = preg_replace('/ class="[^"]*"/', "", $imageElement);
+                    $imageElement = preg_replace('/ alt="[^"]*"/', " alt=\"\"", $imageElement);
+                    $imageElement = preg_replace('/ title="[^"]*"/', "", $imageElement);
+                    $output .= "<figure style=\"max-width:".$width."\" aria-labelledby=\"".$captionId."\"".$class.">\n";
+                    $output .= $imageElement."\n";
+                    $output .= $captionElement;
+                    $output .= "</figure>";
+                } else {
+                    $this->yellow->log("error", "Invalid format of image shortcut!");
+                    $output = $imageElement;
+                }
+            } else {
+                $page->error(500, "Figure requires 'image' extension!");
+            }
+        }
+        return $output;
+    }
+    
+    // Handle page extra data
+    public function onParsePageExtra($page, $name) {
+        $output = null;
+        if ($name=="header") {
+            $extensionLocation = $this->yellow->system->get("coreServerBase").$this->yellow->system->get("coreExtensionLocation");
+            $output = "<link rel=\"stylesheet\" type=\"text/css\" media=\"all\" href=\"{$extensionLocation}figure.css\" />\n";
+        }
+        return $output;
+    }
+}
